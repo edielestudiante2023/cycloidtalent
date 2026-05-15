5448d81df268af72363820b8f198eaf3ac501365
edielestudiante2023 <edielestudiante2023@gmail.com>
2026-05-15 10:59:02 -0500

    feat(analytics): instalar Microsoft Clarity (wrkckt6q8o) en layout principal

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -63,6 +63,15 @@
         gtag('js', new Date());
         gtag('config', 'G-TXM6N4C2CY');
     </script>
+
+    <!-- Microsoft Clarity -->
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "wrkckt6q8o");
+    </script>
 </head>
 <body class="bg-cycloid-bg text-cycloid-text font-sans antialiased" x-data>
 
